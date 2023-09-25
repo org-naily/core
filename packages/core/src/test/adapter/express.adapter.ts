7cb7895d7b6de2da8be3f7d4823aaf3f021f68b6
Zero的Mac <1203970284@qq.com>
2023-09-25 14:06:50 +0800

    feat: update

--- packages/core/src/test/adapter/express.adapter.ts
+++ packages/core/src/test/adapter/express.adapter.ts
@@ -2,7 +2,7 @@
  * @Author: Zero的Mac 1203970284@qq.com
  * @Date: 2023-09-25 00:30:56
  * @LastEditors: Zero的Mac 1203970284@qq.com
- * @LastEditTime: 2023-09-25 14:04:20
+ * @LastEditTime: 2023-09-25 14:06:40
  * @FilePath: /v5/packages/core/src/test/adapter/express.adapter.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
