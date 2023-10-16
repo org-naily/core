ad63e03557e234187f2de8ed6d4113a3d9358e45
Zero的Mac <1203970284@qq.com>
2023-10-16 14:10:24 +0800

    feat: commit

--- packages/test/src/main.controller.ts
+++ packages/test/src/main.controller.ts
@@ -1,9 +1,12 @@
+import { Bean } from "@naily/core";
 import { Controller, Get } from "@naily/web";
+import { ListenService } from "./listen.service";
 
 @Controller()
 export class MainController {
   @Get()
-  getHello() {
+  @Bean([ListenService], [ListenService])
+  public getHello() {
     return "hello world";
   }
 }
