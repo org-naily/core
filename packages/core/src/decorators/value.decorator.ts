36c9beb075acce8246ab8440c7a92247bc4e969d
Zero的Mac <1203970284@qq.com>
2023-10-17 11:50:28 +0800

    feat: add @Value

--- packages/core/src/decorators/value.decorator.ts
+++ packages/core/src/decorators/value.decorator.ts
@@ -1,6 +1,6 @@
 import { WATERMARK } from "../constants/watermark.constant";
 
-export function Value(jexl: string) {
+export function Value<T extends string>(jexl?: T) {
   return (target: Object, propertyKey: string | symbol) => {
     Reflect.defineMetadata(WATERMARK.VALUE, jexl, target, propertyKey);
   };
