8dfde460ff54be0fb42ad5970a4b51ef584ef0a9
Zero的Mac <1203970284@qq.com>
2023-10-19 19:08:13 +0800

    docs: add @Value

--- packages/core/src/decorators/value.decorator.ts
+++ packages/core/src/decorators/value.decorator.ts
@@ -9,7 +9,6 @@ export function Value<T extends string>(jexl?: T, dynamic: boolean = false) {
       get: function () {
         return dynamic ? NailyConfigure.getConfigureByJexlDynamic(jexl) : NailyConfigure.getConfigureByJexl(jexl);
       },
-      writable: false,
     });
   };
 }
