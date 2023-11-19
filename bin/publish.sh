53911230d8e5a4e2bac8314cb2cec6803038a270
Zero的Mac <1203970284@qq.com>
2023-11-20 01:39:03 +0800

    feat: update npm

--- bin/publish.sh
+++ bin/publish.sh
@@ -1,4 +1,4 @@
 pnpm -F @org-naily/core publish
 pnpm -F @org-naily/backend publish
-pnpm -F @org-naily/express publish
-pnpm -F @org-naily/koa publish
+pnpm -F @org-naily/backend-express publish
+pnpm -F @org-naily/backend-koa publish
